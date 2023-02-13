5f7c466f8689bfdc64bdf22f0472ecb9f78453d1
Michael Hogan <mjhogan38@gmail.com>
2023-02-13 01:03:17 -0600

    Answered other questions and added top-down (recursive) approach.

--- Rectangle Cutting/RectangleCutting.py
+++ Rectangle Cutting/RectangleCutting.py
@@ -1,12 +1,21 @@
-# Given an a*b rectangle, your task is to cut it into squares. On each move you can select a rectangle and cut it into two rectangles in such a way that all side lengths remain integers. What is the minimum possible number of moves?
-# 
-#   ----------------------
-#   |                    |
-# a |                    |
-#   |                    |
-#   ----------------------
-#             b
-import time
+#=================================================================
+# Rectangle Cutting Problem (https://cses.fi/problemset/task/1744)
+# Solution by M.Hogan (V674R446)
+#=================================================================
+# a.) State Space:
+#           The state space of the MDP for the Rectangle Cutting Problem consists of two variables: the height and the width of the given rectangle. 
+#           Within the memoization table, `a` corresponds to the height/first inputted dimension, and `b` corresponds to the width/second inputted dimension.
+#     Action Space:
+#           The action space of the MDP for the Rectangle Cutting Problem consists of all the possible cuts that may be made to divide the current rectangle into two rectangles that each have a height and width that are both positive integers, to the point that the original rectangle is entirely composed of squares
+#           These options may divide the height OR the width (but not both) of a rectangle into two smaller segments. For each rectangle, there are (height+width-2) possible actions that may be taken.
+#     Reward Space:
+#            The reward space of the MDP for the Rectangle Cutting Problem consists of a "-1" for each cut that is made, where the goal is to maximize the score while still achieving the endgoal.
+#
+# b.) Yes, this is indeed a substring problem as the defined approach begins by finding the optimal number of cuts for each possible sub-rectangle (or substring, if you will) that may result from a cut at a given height/width, and the results are then combined to find the optimal solution for the original rectangle. It is not a prefix/suffix problem, as there is no clear place to start.
+#
+# c.) See below.
+
+import numpy
 
 def ParseInput(inputString):
     inputList = inputString.split() # Split the input string on ' ' characters.
@@ -15,7 +24,7 @@ def ParseInput(inputString):
     return inputList[0], inputList[1]
 
 
-def CutRectangle(a, b):
+def IterativeCutRectangle(a, b):
     # Create an array to memoize all the optimal rectangle cutting solutions.
     memoizationList = [[0 for i in range(b+1)] for j in range(a+1)]
 
@@ -51,13 +60,57 @@ def CutRectangle(a, b):
                 # Add 1 to the optimal solutions of the sub-problems to get the min number of cuts for a rectangle of size height*width
                 memoizationList[height][width] = minNumberOfCuts
 
+    # Return the optimal (minimal) number of cuts for a rectangle of height a and width b.
     return memoizationList[a][b]
 
+memoizationArray = numpy.ones((501, 501)) * -1
+
+def RecursiveCutRectangle(a,b):
+    # Base case (given sides form a square):
+    if (a == b):
+        return 0
+    
+    # Secondary case (x*y rectangle where y==1 will require x-1 cuts to yield x squares):
+    if (a == 1):
+        return b - 1
+    if (b == 1):
+        return a - 1
+    
+    # If it's already been calculated:
+    if (memoizationArray[a][b] > 0):
+        return memoizationArray[a][b]
+
+    # Otherwise, look for the best solution:
+    # Find the optimal cut that can be made to divide a into two pieces.
+    for n in range(1, (a+2)//2):
+        numCuts = RecursiveCutRectangle(a-n, b) + RecursiveCutRectangle(n, b) + 1
+        if (numCuts < memoizationArray[a][b] or memoizationArray[a][b] < 0):
+            memoizationArray[a][b] = numCuts
+    
+    # Find the optimal cut that can be made to divide b into two pieces, and keep it if it's better than the optimal cut that can be made to divide a into two pieces.
+    for n in range(1, (b+2)//2):
+        numCuts = RecursiveCutRectangle(a, b-n) + RecursiveCutRectangle(a, n) + 1
+        if (numCuts < memoizationArray[a][b]):
+            memoizationArray[a][b] = numCuts
+    
+    # Return the optimal (minimal) number of cuts for a rectangle of height a and width b.
+    return int(memoizationArray[a][b])
+
+    
+
 if (__name__ == "__main__"):
-    startTime = time.perf_counter()
     inputString = input()
     a, b = ParseInput(inputString)
-    print(CutRectangle(a, b))
-    stopTime = time.perf_counter()
-    runTime = stopTime - startTime
-    print(f"Program ran in {runTime} seconds!")
+    print(IterativeCutRectangle(a, b))
+    print("\n===================\n")
+    print(RecursiveCutRectangle(a, b))
+
+
+# d.) (Will be present in document...)
+# 
+# e.) Time Complexity The most expensive computation is the set of nested for loops in the `CutRectangle()` function.
+#     The outermost `for` loop executes `a` times, where a is the first given integer.
+#     The second-outermost `for` loop executes `b` times, where b is the second given integer.
+#     There are two `for` loops nested within the immediately aforementioned `for` loop, the first of which executes in O(a) time and the second of which executes in O(b) time.
+#     Thus, the total runtime can be described as O(a)*O(b)*(O(a)+O(b)), or:    
+#           O(a*b*(a+b))
--- Removal Game/RemovalGame.py
+++ Removal Game/RemovalGame.py
@@ -1,3 +1,22 @@
+#=================================================================
+# Removal Game Problem (https://cses.fi/problemset/task/1097)
+# Solution by M.Hogan (V674R446)
+#=================================================================
+# a.) State Space:
+#           The state space of the MDP for the Removal Game Problem consists of a total of two variables: the number of values in the given list (represented by `numberOfElements`) and the list of values (represented by `gameList`).
+#    
+#     Action Space:
+#           The action space of the MDP for the Removal Game Problem consists of choosing to remove the first item or the last item in the given list.
+#
+#     Reward Space:
+#            The reward space of the MDP for the Removal Game Problem consists of a "+gameList[x]", where gameList[x] is the value associated with the item removed from the list as a result of the selected action.
+#
+# b.) Yes, this is a substring problem, as the defined approach begins with solving for the optimal solution of each sub-list of elements, gradually expanding the number of elements in that sub-list so that the optimal solution for each possible sub-list (or substring, if you will) of the original list is found. It is not a prefix/suffix problem, as there is no clear place to start.
+#
+# c.) See below.
+
+import numpy
+memoizationArray = numpy.ones((5001, 5001)) * -1
 
 def ParseInput():
     # Get the inputs:
@@ -12,10 +31,10 @@ def ParseInput():
     # Return the number of elements in the list as well as the list itself.
     return numberOfElements, elementList
 
-def OptimalRemovalGameScore(numberOfElements, gameList):
+def BottomUpOptimalRemovalGameScore(numberOfElements, gameList):
     # Allocate the appropriate amount of memory for the memoization
-    memoizationList = [[0 for i in range(0, numberOfElements)] for j in range(0, numberOfElements)]
-
+    # memoizationList = [[0 for i in range(0, numberOfElements)] for j in range(0, numberOfElements)]
+    memoizationList = numpy.zeros((5001, 5001))
     # This is the big time-suck. Runs in O(n^2), where n is the given number of elements in the given list.
     for distance in range(0, numberOfElements):
         for i in range(distance, numberOfElements):
@@ -41,6 +60,27 @@ def OptimalRemovalGameScore(numberOfElements, gameList):
     # Return the value of the optimal choice for the entire list.
     return memoizationList[0][-1]
 
+
+def RecursiveOptimalRemovalGameScore(startIndex, stopIndex, gameList):
+    if (stopIndex - startIndex == 1):
+        # Choose the option (of the two choices) that contains the most points, as there is nothing else about which you need to worry.
+        return max(gameList[startIndex], gameList[stopIndex])
+    else:
+        # Find the choice with the maximum number of points associated with all remaining elements given that your opponent will choose the next most optimal choice.
+        memoizationArray[startIndex][stopIndex] = max(gameList[startIndex] + min(RecursiveOptimalRemovalGameScore(startIndex+2, stopIndex, gameList), RecursiveOptimalRemovalGameScore(startIndex+1, stopIndex-1, gameList)),
+                                                        gameList[stopIndex] + min(RecursiveOptimalRemovalGameScore(startIndex, stopIndex-2, gameList), RecursiveOptimalRemovalGameScore(startIndex+1, stopIndex-1, gameList)))
+        # Return the number of points associated with that choice.                                                
+        return int(memoizationArray[startIndex][stopIndex])
+
+
 if (__name__ == "__main__"):
     numberOfElements, elementList = ParseInput()
-    print(OptimalRemovalGameScore(numberOfElements, elementList))
+    print("Bottom-Up DP Removal Game Max Score: ")
+    print(BottomUpOptimalRemovalGameScore(numberOfElements, elementList))
+    print("\nRecursive DP Removal Game Max Score: ")
+    print(RecursiveOptimalRemovalGameScore(0, numberOfElements - 1, elementList))
+
+    
+# d.) (Will be present in document...)
+#
+# e.) The runtime of this algorithm is O(n^2), where n == numberOfElements. This is because the for loops must iterate over each element, calculating the optimal solution for a sublist that expands to include each other element that appears later in the list than the first element.
